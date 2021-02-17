5d7f1e94329ba6244289f70cf03304013e8036f8
Joseph Aist <66432518+JoeyAist@users.noreply.github.com>
2021-02-17 09:52:18 -0700

    Create feature_extraction.m
    This is the function to extract three features from each row. It is set up to read through an entire "adjusted_data_decembercal" file

---
+++ Insect-Lidar/feature_extraction.m
@@ -0,0 +1,30 @@
+function features = feature_extraction(file_path)
+% This function extracts three features per row of an image and
+% concatinates the features of an image vertically. 
+%
+% file_path should be the path to the "adjusted_data_decembercal" of the
+% folder whos features are being extracted
+%
+% Created by Joseph Aist
+
+load(file_path);
+
+features = zeros(length(adjusted_data_decembercal)*178,3);
+
+for i = 1:length(adjusted_data_decembercal)
+
+    A = adjusted_data_decembercal(i).normalized_data;
+
+    Azeromean = abs(A - repmat(mean(A,2),1,size(A,2))); % Take mean of each row and take absolute value
+    row_mean = mean(A,2);
+    image_mean = mean(A(:));
+    filtered_row_std = std(Azeromean')';
+    first_diff = max(abs(diff(Azeromean')))';
+    
+    im_features = [(row_mean - image_mean), filtered_row_std, first_diff];
+
+    features((i-1)*178+1:i*178,1) = im_features(:,1);
+    features((i-1)*178+1:i*178,2) = im_features(:,2);
+    features((i-1)*178+1:i*178,3) = im_features(:,3);
+
+end 
