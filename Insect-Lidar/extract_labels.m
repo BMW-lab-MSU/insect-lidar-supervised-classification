901b819a020e1f5c69b6d82d6b918c7ded02eadf
Trevor Vannoy <trevorvannoy@montana.edu>
2021-02-18 15:36:03 -0700

    fix label vector dimensions

--- Insect-Lidar/extract_labels.m
+++ Insect-Lidar/extract_labels.m
@@ -6,8 +6,10 @@ function labels = extract_labels(insects, datastruct)
 %   element of datastruct. datastruct is expected to be from an
 %   adjusted_data_decembercal.mat file. insects needs to be the
 %   insects field of the fftcheck.mat file associated with datastruct. 
+%
+%   Example: labels = extract_labels(fftcheck.insects, adjusted_data_decembercal)
 
-dims = size(datastruct(1).normalized_data);
+[nrows, ncolumns] = size(datastruct(1).normalized_data);
 labels = cell(size(datastruct));
 
 % by construction, the first part of the filename for all structs in 
@@ -18,7 +20,7 @@ folder_name = temp{1};
 % datastruct contains multiple images, each for a different pan/tilt angle
 % combination
 for image_num = 1:numel(datastruct)
-    image_labels = zeros(dims);
+    image_labels = zeros(nrows, 1);
 
     for insect_num = 1:numel(insects)
 
