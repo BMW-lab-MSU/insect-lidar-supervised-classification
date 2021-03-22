f16d7f8d79db43b1dc8007ba39a101be442d74ac
Trevor Vannoy <trevorvannoy@montana.edu>
2021-03-22 13:41:43 -0600

    extract features per image instead of per adjusted_data_decembercal file

--- feature_extraction.m
+++ feature_extraction.m
@@ -1,4 +1,4 @@
-function features = feature_extraction(file_path)
+function features = feature_extraction(X)
 % This function extracts three features per row of an image and
 % concatinates the features of an image vertically. 
 %
@@ -7,41 +7,27 @@ function features = feature_extraction(file_path)
 %
 % Created by Joseph Aist
 
-% Load the adjusted_data_decembercal data for the set of data whose
-% features are being extracted.
-load(file_path);
-
 % Obtain the number of files from adjusted_data_decembercal and multiply
 % the number of files by the height of the files for to find the number of
 % rows to extract the features from.
-rows = length(adjusted_data_decembercal)*178;
-number_features = 3;
 
-features = zeros(rows,number_features);
+rows = height(X);
+nfeatures = 3;
 
-for i = 1:length(adjusted_data_decembercal)
+features = zeros(rows, nfeatures);
 
-    A = adjusted_data_decembercal(i).normalized_data;
+% Extract the three features.
 
-    % Extract the three features.
+% Subtract the image mean from the row means.
+row_mean = mean(X,2);
+image_mean = mean(X(:));
 
-    % Subtract the image mean from the row means.
-    row_mean = mean(A,2);
-    image_mean = mean(A(:));
+% Take the standard deviation of the filtered rows
+filtered_row_std = std(X, 0, 2);
 
-    % Take the standard deviation of the filtered rows
-    filtered_row_std = std(A, 0, 2);
+% Take max difference from each row
+first_diff = max(abs(diff(X, 1, 2)), [], 2);
 
-    % Take max difference from each row
-    first_diff = max(abs(diff(A, 1, 2)), [], 2);
-    
-    im_features = [(row_mean - image_mean), filtered_row_std, first_diff];
+features = [(row_mean - image_mean), filtered_row_std, first_diff];
 
-    for k = 1:number_features
-        % Assign the current features to their locations within the feature
-        % array.
-        features((i-1)*178+1:i*178,k) = im_features(:,k);
-    end
-            
-end 
 end
