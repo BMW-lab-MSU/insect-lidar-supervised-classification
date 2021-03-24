8096841002940c570afb6c4217a661f5ff109b57
Trevor Vannoy <trevorvannoy@montana.edu>
2021-03-24 13:36:41 -0600

    compute how many bins are required to contain 90% of the energy

--- feature_testing.m
+++ feature_testing.m
@@ -27,23 +27,13 @@ load('InsectLidarDataTutorial/exampledata.mat')
 
 data = exampledata(1).normalized_data;
 
-psd = abs(fft(data, [], 2)).^2;
+X = fft(data, [], 2);
+psd = abs(X).^2;
 psd = psd(:,1:end/2);
 
+normalized_psd = psd./psd(:,1);
 
-figure
-imagesc(data)
-title('original data: insect at row 97')
-figure
-imagesc(psd, [0, 100])
-title('psd')
-
-
-figure
-plot(psd(160,:))
-hold on 
-plot(psd(97,:), 'LineWidth', 3)
-title('psd')
+spectralphase = angle(X);
 
 
 avg_psd = mean(psd, 2);
@@ -52,6 +42,47 @@ median_psd = median(psd, 2);
 mad_psd = mad(psd, 1, 2);
 skew_psd = skewness(psd, 1, 2);
 kurtosis_psd = kurtosis(psd, 1, 2);
+
+
+% compute the index at which 90% of energy is contained
+energypct = cumsum(psd,2)./sum(psd,2);
+energy90pct = zeros(1, height(psd));
+energy90pct_no_dc = zeros(1, height(psd));
+
+for row = 1:height(psd)
+    % ignore dc component because it contains basically all the energy 
+    % NOTE: actually, ignoring the dc component might not be important;
+    %       in this case, removing the dc component only makes a difference
+    %       of 1 index at most
+    energy90pct(row) = find(energypct(row,1:end) >= 0.90, 1);
+    energy90pct_no_dc(row) = find(energypct(row,2:end) >= 0.90, 1);
+
+end
+
+
+figure
+imagesc(data)
+title('original data: insect at row 97')
+
+figure
+imagesc(psd, [0, 100])
+title('psd')
+
+% figure
+% imagesc(spectralphase)
+% title('phase')
+
+% figure
+% plot(spectralphase(97,:))
+% hold on
+% plot(spectralphase(160,:))
+
+figure
+plot(psd(160,:))
+hold on 
+plot(psd(97,:), 'LineWidth', 3)
+title('psd')
+
 
 figure
 plot(avg_psd)
@@ -80,3 +111,14 @@ title('kurtosis of psd')
 figure
 plot(sum(psd, 2))
 title('total energy')
+
+figure
+boxplot(quantile(data([97,160],:), [0.25, 0.5, 0.75], 2)')
+xticklabels({'insect', 'hard object'})
+
+figure
+plot(energy90pct)
+hold on
+plot(energy90pct_no_dc)
+title('90% energy index')
+
