787e1d48196ab5232304a09b5f0c6dc8d5e1946e
Trevor Vannoy <trevorvannoy@montana.edu>
2021-03-24 09:51:34 -0600

    look at statistics of psd

--- feature_testing.m
+++ feature_testing.m
@@ -21,16 +21,49 @@ disp(pks(2)*1024/2);
 max_loc = find(Y == max(Y))
 
 
-%%
+%% Trevor's dumb tests
+close all
 load('InsectLidarDataTutorial/exampledata.mat')
 
 data = exampledata(1).normalized_data;
 
 psd = abs(fftshift(fft(data, [], 2), 2)).^2;
 
+% we are only interested on plotting the positive frequencies,
+% so we start at end/2; however, the dc component dominates, so
+% we cut that out by starting at end/2 + 5 (arbitrarily)
+figure
+imagesc(data)
+title('original data: insect at row 97')
+figure
 imagesc(psd(:,end/2+5:end), [0, 100])
+title('psd')
+
 
 figure
 plot(psd(160,end/2+5:end))
 hold on 
 plot(psd(97,end/2+5:end), 'LineWidth', 3)
+title('psd')
+
+
+avg_psd = mean(psd, 2);
+std_psd = std(psd, 0, 2);
+median_psd = median(psd, 2);
+mad_psd = mad(psd, 1, 2);
+
+figure
+plot(avg_psd)
+title('mean psd')
+
+figure
+plot(std_psd)
+title('std psd')
+
+figure
+plot(median_psd)
+title('median psd')
+
+figure
+plot(mad_psd)
+title('median absolute deviation of psd')
