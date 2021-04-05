bbfde86a6eacfb8f01595814e01e6e59b1e99e5b
Trevor Vannoy <trevorvannoy@montana.edu>
2021-04-05 11:13:20 -0600

    use histograms instead of pairwise scatter plot becaues they are easier to see

--- visualize_feature_distributions.m
+++ visualize_feature_distributions.m
@@ -1,14 +1,23 @@
 function visualize_feature_distributions(features, labels, feature_names)
 
-group = categorical(labels, [0, 1], {'Non-insect', 'Insect'});
+groups = categorical(labels, [0, 1], {'Non-insect', 'Insect'});
 
 % plot properties
 % color = [[178,223,138]; [31,120,180]]/255;
-color = [];
-markers = 'od';
-sizes = [];
+% color = [];
+% markers = 'od';
+% sizes = [];
 
-figure
-[h, ax, bigax] = gplotmatrix(features, [], group, color, markers, sizes, [], 'grpbars', feature_names);
+% figure
+% [h, ax, bigax] = gplotmatrix(features, [], group, color, markers, sizes, [], 'grpbars', feature_names);
 
+tlayout = tiledlayout('flow');
+
+nFeatures = width(features);
+
+for n = 1:nFeatures
+    nexttile
+    ghistogram(features(:, n), groups)
+    title(feature_names(n));
 end
+legend(categories(groups))
