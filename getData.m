4a300ba61fbce4763e059a8c9308e90da8860573
rustky <50724477+rustky@users.noreply.github.com>
2021-04-21 10:32:40 -0600

    bug fix

--- getData.m
+++ getData.m
@@ -1,8 +1,10 @@
 function data = getData(basefilepath,day,folder,image)
-    load([basefilepath '\' day '\' folder '\' 'adjusted_data_decembercal.mat']);
+    load([basefilepath filesep day filesep folder filesep 'adjusted_data_decembercal.mat']);
     for i = 1:length(adjusted_data_decembercal)
-        if strcmp([folder '\' image '.mat'],adjusted_data_decembercal(i).filename)
+%         disp([folder '/' image]); disp(adjusted_data_decembercal(i).filename);
+        if strcmp(string([folder '/' image]),string(adjusted_data_decembercal(i).filename)) == true
             data = adjusted_data_decembercal(i).normalized_data;
+            break;
         end
     end
 end
--- randomizeData.m
+++ randomizeData.m
@@ -1,4 +1,4 @@
 function [train,test] = randomizeData(data_size,train_size)
 all_indicies = 1:data_size;
-train = randsample(data_size, train_size);
+train = randsample(data_size, train_size)';
 test = setdiff(all_indicies,train);
