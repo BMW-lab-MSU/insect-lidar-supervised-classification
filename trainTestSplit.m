fb13d6d11477df2b6c54c80be9e4b150a24865ed
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-25 12:08:12 -0600

    split data by scan

---
+++ trainTestSplit.m
@@ -0,0 +1,54 @@
+%% Setup
+clear
+
+% Set random number generator properties for reproducibility
+rng(0, 'twister');
+
+datadir = '/home/trevor/research/afrl/data/Data_2020_Insect_Lidar/MLSP-2021';
+datafile = 'scans.mat';
+
+%% Load data
+load([datadir filesep datafile])
+
+
+%% Extract features
+scanFeatures = cell(numel(scans), 1);
+
+for i = progress(1:numel(scans))
+    scanFeatures{i} = cellfun(@(X) extractFeatures(X), scans(i).Data, ...
+        'UniformOutput', false);
+end
+
+%%
+labels = {scans.Labels}';
+scanLabels = vertcat(scans.ScanLabel);
+
+%% Partition into training and test sets
+TEST_PCT = 0.2;
+
+holdoutPartition = cvpartition(scanLabels, 'Holdout', TEST_PCT, 'Stratify', true);
+
+
+% trainingData = {scans(training(holdoutPartition)).Data}';
+% testingData = {scans(test(holdoutPartition)).Data}';
+trainingData = scanFeatures(training(holdoutPartition));
+testingData = scanFeatures(test(holdoutPartition));
+trainingLabels = labels(training(holdoutPartition));
+testingLabels = labels(test(holdoutPartition));
+
+%% Partition the data for k-fold cross validation
+N_FOLDS = 5;
+
+crossvalPartition = cvpartition(scanLabels(training(holdoutPartition)), ...
+    'KFold', N_FOLDS, 'Stratify', true);
+
+
+%% Save training and testing data
+mkdir(datadir, 'testing');
+save([datadir filesep 'testing' filesep 'testingData.mat'], ...
+    'testingData', 'testingLabels', 'holdoutPartition', 'scanLabels', '-v7.3');
+
+mkdir(datadir, 'training');
+save([datadir filesep 'training' filesep 'trainingData.mat'], ...
+    'trainingData', 'trainingLabels', 'crossvalPartition', ...
+    'holdoutPartition', 'scanLabels', '-v7.3');
