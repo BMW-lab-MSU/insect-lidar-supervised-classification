c540cef497c4575cedc286abcc0d5d0947ab8709
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-26 21:06:21 -0600

    tune sampling for RUSBoost

---
+++ tuneSamplingRUSBoost.m
@@ -0,0 +1,24 @@
+%% Setup
+rng(0, 'twister');
+
+datadir = '/home/trevor/research/afrl/data/Data_2020_Insect_Lidar/MLSP-2021';
+
+if isempty(gcp('nocreate'))
+    parpool();
+end
+
+%% Load data
+load([datadir filesep 'training' filesep 'trainingData.mat']);
+
+%% Tune sampling ratios
+result = tuneSamplingBase(@rusboost, trainingFeatures, trainingData, ...
+    trainingLabels, scanLabels, crossvalPartition, ...
+    'Progress', true, 'UseParallel', true);
+
+save([datadir filesep 'training' filesep 'samplingTuningAdaboost.mat'], 'result')
+
+%% Model fitting function
+function model = rusboost(data, labels, ~)
+    t = templateTree('Reproducible',true);
+    model = compact(fitcensemble(data, labels, 'Method', 'RUSBoost', 'Learners', t, 'ScoreTransform', 'doublelogit'));
+end
