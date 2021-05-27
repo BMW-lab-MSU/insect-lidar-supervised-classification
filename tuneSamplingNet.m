3b13a9e577d059ed79990bf47915f10fa87ced98
Trevor Vannoy <trevorvannoy@montana.edu>
2021-05-27 13:12:59 -0600

    tune sampling for shallow neural network

---
+++ tuneSamplingNet.m
@@ -0,0 +1,24 @@
+%% Setup
+rng(0, 'twister');
+
+datadir = '/home/trevor/research/afrl/data/Data_2020_Insect_Lidar/MLSP-2021';
+
+if isempty(gcp('nocreate'))
+    parpool();
+end
+
+%% Load data
+load([datadir filesep 'training' filesep 'trainingData.mat']);
+
+%% Tune sampling ratios
+result = tuneSamplingBase(@nnet, trainingFeatures, trainingData, ...
+    trainingLabels, scanLabels, crossvalPartition, ...
+    'Progress', true, 'UseParallel', true);
+
+save([datadir filesep 'training' filesep 'samplingTuningNet.mat'], 'result')
+
+%% Model fitting function
+function model = nnet(data, labels, ~)
+    model = compact(fitcnet(data, labels, 'LayerSizes', [25], ...
+        'Standardize', true));
+end
