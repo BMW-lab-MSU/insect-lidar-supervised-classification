b6b4220ac08523ad8090010f824cb82b18f2af61
Trevor Vannoy <trevorvannoy@montana.edu>
2021-06-10 12:34:21 -0600

    add other classifiers to testing script

--- testClassifiers.m
+++ testClassifiers.m
@@ -55,3 +55,90 @@ disp(adaBoost.Row)
 disp('Image results')
 disp(adaBoost.Image.Confusion)
 disp(adaBoost.Image)
+
+%% Test RUSBoost
+disp('Testing RUSBoost....')
+disp('---------------')
+disp('')
+load([datadir filesep 'training' filesep 'models' filesep 'rusboost.mat']);
+
+%%%%%%%%%%%%%%%%%%
+% row results
+%%%%%%%%%%%%%%%%%%
+rusBoost.Row.PredLabels = predict(model, features);
+
+rusBoost.Row.Confusion = confusionmat(labels, rusBoost.Row.PredLabels);
+
+[a, p, r, f2, mcc] = analyzeConfusion(rusBoost.Row.Confusion);
+rusBoost.Row.Accuracy = a;
+rusBoost.Row.Precision = p;
+rusBoost.Row.Recall = r;
+rusBoost.Row.F2 = f2;
+rusBoost.Row.MCC = mcc;
+
+%%%%%%%%%%%%%%%%%%
+% image results
+%%%%%%%%%%%%%%%%%%
+rusBoost.Image.Confusion = imageConfusion(rusBoost.Row.PredLabels, testingLabels, holdoutPartition);
+
+[a, p, r, f2, mcc] = analyzeConfusion(rusBoost.Image.Confusion);
+rusBoost.Image.Accuracy = a;
+rusBoost.Image.Precision = p;
+rusBoost.Image.Recall = r;
+rusBoost.Image.F2 = f2;
+rusBoost.Image.MCC = mcc;
+
+%%%%%%%%%%%%%%%%%%
+% Display results
+%%%%%%%%%%%%%%%%%%
+disp('Row results')
+disp(rusBoost.Row.Confusion)
+disp(rusBoost.Row)
+disp('Image results')
+disp(rusBoost.Image.Confusion)
+disp(rusBoost.Image)
+
+%% Test neural net
+disp('Testing neural net....')
+disp('---------------')
+disp('')
+load([datadir filesep 'training' filesep 'models' filesep 'nnet.mat']);
+
+%%%%%%%%%%%%%%%%%%
+% row results
+%%%%%%%%%%%%%%%%%%
+nnet.Row.PredLabels = predict(model, features);
+
+nnet.Row.Confusion = confusionmat(labels, nnet.Row.PredLabels);
+
+[a, p, r, f2, mcc] = analyzeConfusion(nnet.Row.Confusion);
+nnet.Row.Accuracy = a;
+nnet.Row.Precision = p;
+nnet.Row.Recall = r;
+nnet.Row.F2 = f2;
+nnet.Row.MCC = mcc;
+
+%%%%%%%%%%%%%%%%%%
+% image results
+%%%%%%%%%%%%%%%%%%
+nnet.Image.Confusion = imageConfusion(nnet.Row.PredLabels, testingLabels, holdoutPartition);
+
+[a, p, r, f2, mcc] = analyzeConfusion(nnet.Image.Confusion);
+nnet.Image.Accuracy = a;
+nnet.Image.Precision = p;
+nnet.Image.Recall = r;
+nnet.Image.F2 = f2;
+nnet.Image.MCC = mcc;
+
+%%%%%%%%%%%%%%%%%%
+% Display results
+%%%%%%%%%%%%%%%%%%
+disp('Row results')
+disp(nnet.Row.Confusion)
+disp(nnet.Row)
+disp('Image results')
+disp(nnet.Image.Confusion)
+disp(nnet.Image)
+
+%% Save results
+save([datadir filesep 'testing' filesep 'results'], 'adaBoost, rusBoost', 'nnet');
